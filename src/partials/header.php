5c073ea9f7434c202c496a6ecda22643e264dec8
khavi <vib2105566@student.ctu.edu.vn>
2025-03-11 20:28:42 +0700

    Update header.php

--- src/partials/header.php
+++ src/partials/header.php
@@ -2,8 +2,13 @@
     <div class="container d-flex justify-content-between align-items-center">
         <h1 class="h4 text-white mb-0">Nhà Thuốc</h1>
         <nav>
+            <?php if (isset($_SESSION["username"])): ?>
+            <a href="account.php" class="btn btn-light me-2">Tài khoản</a>
+            <a href="logout.php" class="btn btn-light text-danger">Đăng xuất</a>
+            <?php else: ?>
             <a href="login.php" class="btn btn-light me-2">Đăng nhập</a>
             <a href="register.php" class="btn btn-light text-primary">Đăng ký</a>
+            <?php endif; ?>
         </nav>
     </div>
 </header>
